7b18cb3612326f3011bf7708783391e3a58b921d
arimafp <ari_khvalko@mail.ru>
2023-12-28 12:41:40 +0300

    add: pdf render

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { Document, Page } from 'react-pdf'
 
-const PdfRenderer = () => {
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
+interface PdfRendererProps {
+  url: string
+}
+
+const PdfRenderer = ({url}: PdfRendererProps) => {
   return (
     <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
       <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
@@ -8,6 +16,17 @@ const PdfRenderer = () => {
           top bar
         </div>
       </div>
+
+
+      <div className='flex-1 w-full max-h-screen'>
+        <div>
+          <Document className='max-h-full'>
+            <Page pageNumber={1} />
+          </Document>
+        </div>
+      </div>
+
+
     </div>
   )
 }
